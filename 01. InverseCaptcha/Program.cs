3dcef1ff41f069964879488bd825c2a62f1af525
Pariador <pariador.corterian@gmail.com>
2017-12-01 20:53:17 +0200

    Solve InverseCaptcha

---
+++ 01. InverseCaptcha/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace InverseCaptcha
+{
+    using System;
+    using System.IO;
+
+    internal static class Program
+    {
+        internal static int ToInt(char digit)
+        {
+            if (!char.IsDigit(digit))
+            {
+                throw new ArgumentException("Char must be a digit.");
+            }
+
+            return digit - '0';
+        }
+
+        internal static long SumRepeatingDigits(string number)
+        {
+            long sum = 0;
+
+            for (int i = 0; i < number.Length - 1; i++)
+            {
+                if (number[i] == number[i + 1])
+                {
+                    sum += ToInt(number[i]);        
+                }
+            }
+
+            if (number[number.Length - 1] == number[0])
+            {
+                sum += ToInt(number[0]);
+            }
+
+            return sum;
+        }
+
+        internal static void Main()
+        {
+            string puzzleInput = null;
+
+            using (var reader = new StreamReader("input.txt"))
+            {
+                puzzleInput = reader.ReadToEnd();
+            }
+
+            while (true)
+            {
+                Console.Write(">");
+
+                string input = Console.ReadLine().ToLower();
+                if (input == "exit")
+                {
+                    break;
+                }
+                else if (input == "puzzle")
+                {
+                    input = puzzleInput;
+                }
+
+                long sum = SumRepeatingDigits(input);
+
+                Console.WriteLine(sum + Environment.NewLine);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# My Solutions To Advent of Code 2017
+[Advent of Code](http://adventofcode.com/2017)
