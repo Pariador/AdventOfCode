7360da9af5387f70c3a482ca8b0533c4fc16b9f0
Pariador <pariador.corterian@gmail.com>
2017-12-18 21:13:22 +0200

    Solve Duet

---
+++ 18.Duet/InstructionType.cs
@@ -0,0 +1,17 @@
+﻿namespace Duet
+{
+    using System;
+
+    public enum InstructionType
+    {
+        Set = 0,
+        Add = 1,
+        Multiply = 2,
+        Modulo = 3,
+        ConditionalJump = 4,
+        Play = 5,
+        Recover = 6,
+        Send = 7,
+        Recieve = 8
+    }
+}
---
+++ 18.Duet/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Duet
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            string input = null;
+            using (var reader = new StreamReader("input.txt"))
+            {
+                input = reader.ReadToEnd();
+            }
+
+            Instruction[] firstInstructions = ReadInstructions(new StringReader(input));
+            Instruction[] secondInstructions = ReadInstructions(new StringReader(input), sendAndRecieve: true);
+
+            Computer first = new Computer();
+            first.Execute(firstInstructions);
+
+            Console.WriteLine($"First result: {first.FirstRecoveredSound}");
+
+            first.Reset();
+            Computer second = new Computer();
+            first.Partner = second;
+
+            while ((!first.Halt && !first.Waiting) || (!second.Halt && !second.Waiting))
+            {
+                first.Execute(secondInstructions);
+                second.Execute(secondInstructions);
+            }
+
+            Console.WriteLine($"Second result: {second.MessagesSent}");
+        }
+
+        public static Instruction[] ReadInstructions(TextReader reader, bool sendAndRecieve = false)
+        {
+            var instructions = new List<Instruction>();
+
+            while (true)
+            {
+                string line = reader.ReadLine();
+                if (line == null)
+                {
+                    break;
+                }
+
+                Instruction instruction = Instruction.Parse(line, sendAndRecieve);
+
+                instructions.Add(instruction);
+            }
+
+            return instructions.ToArray();
+        }
+    }
+}
