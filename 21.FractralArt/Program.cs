311675f3306a584c598c3a3cc246dacb04068bfd
Pariador <pariador.corterian@gmail.com>
2017-12-30 19:59:30 +0200

    Solve FractalArt

--- 21.FractralArt/Program.cs
+++ 21.FractralArt/Program.cs
@@ -9,20 +9,24 @@
         public static void Main()
         {
             char[,] image = InitImage();
-            var rules = ReadRules("test.txt");
 
-            PrintImage(image);
+            var rules = ReadRules("input.txt");
+            rules = ExpandRules(rules);
 
-            Transformations.FlipHorizontal(image);
-            PrintImage(image);
+            Transformer transformer = new Transformer(image, rules);
 
-            Transformations.FlipVertical(image);
-            PrintImage(image);
+            transformer.Enhance(5);
+            int onesCount = CountOnes(transformer.Image);
+            Console.WriteLine($"Ones Count for 5 itereations: {onesCount}");
+
+            transformer.Enhance(13);
+            onesCount = CountOnes(transformer.Image);
+            Console.WriteLine($"Ones Count for 18 itereations: {onesCount}");
         }
 
         public static Dictionary<char[,], char[,]> ReadRules(string file)
         {
-            var rules = new Dictionary<char[,], char[,]>();
+            var rules = new Dictionary<char[,], char[,]>(new MatrixComparer<char>());
 
             using (var reader = new StreamReader(file))
             {
@@ -41,6 +45,45 @@
             }
 
             return rules;
+        }
+
+        public static Dictionary<char[,], char[,]> ExpandRules(Dictionary<char[,], char[,]> @base)
+        {
+            var expanded = new Dictionary<char[,], char[,]>(@base, new MatrixComparer<char>());
+
+            foreach (var pair in @base)
+            {
+                var variants = GetVariants(pair.Key);
+
+                foreach (var variant in variants)
+                {
+                    expanded[variant] = pair.Value;
+                }
+            }
+
+            return expanded;
+        }
+
+        public static List<char[,]> GetVariants(char[,] image)
+        {
+            var variants = new List<char[,]>();
+
+            variants.Add(Transformer.FlipHorizontal(image));
+            variants.Add(Transformer.FlipVertical(image));
+
+            var last = image;
+            for (int i = 0; i < 3; i++)
+            {
+                var @new = Transformer.Rotate(last);
+
+                variants.Add(@new);
+                variants.Add(Transformer.FlipHorizontal(@new));
+                variants.Add(Transformer.FlipVertical(@new));
+
+                last = @new;
+            }
+
+            return variants;
         }
 
         public static char[,] ToImage(string str)
@@ -68,6 +111,24 @@
                 { '.', '.', '#' },
                 { '#', '#', '#' }
             };
+        }
+
+        public static int CountOnes(char[,] image)
+        {
+            int count = 0;
+
+            for (int row = 0; row < image.GetLength(0); row++)
+            {
+                for (int col = 0; col < image.GetLength(1); col++)
+                {
+                    if (image[row, col] == '#')
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
         }
 
         public static void PrintImage(char[,] image)
