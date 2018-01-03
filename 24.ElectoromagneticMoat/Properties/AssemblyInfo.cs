dd9177e96e564014bd66973230e3955b775e84ea
Pariador <pariador.corterian@gmail.com>
2018-01-03 16:23:53 +0200

    Improve Electromagnetic Moat Solution

--- 24.ElectoromagneticMoat/Properties/AssemblyInfo.cs
+++ 24.ElectoromagneticMoat/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("23.ElectoromagneticMoat")]
+[assembly: AssemblyTitle("ElectoromagneticMoat")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("23.ElectoromagneticMoat")]
+[assembly: AssemblyProduct("ElectoromagneticMoat")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
