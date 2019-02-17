b71e3f68aae72ea055f2cd120f2f86601dec9e5b
Pariador <pariador.corterian@gmail.com>
2019-02-17 17:48:23 +0200

    Solve 2018 - 05.Alchemical Reduction.

---
+++ 2018/05.AlchemicalReduction/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace AlchemicalReduction
+{
+    using System;
+    using System.IO;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    static class Program
+    {
+        static void Main()
+        {
+            char[] polymer = File.ReadAllText("input.txt")
+                .Trim()
+                .ToCharArray();
+
+            int fullyReactedPolymerLength = FullyReact(polymer).Length;
+            Console.WriteLine($"Polymer Length: {fullyReactedPolymerLength}");
+
+
+            Console.WriteLine("\nFinding shortest polymer...");
+            int shortestPolymer = FindShortestPolymer(polymer);
+            Console.WriteLine($"Shortest Polymer: {shortestPolymer}");
+        }
+
+        static char[] Trigger(char[] polymer, out bool triggered)
+        {
+            const char Destroyed = '*';
+
+            triggered = false;
+
+            for (int i = 0; i < polymer.Length - 1; i++)
+            {
+                if ((char.ToLower(polymer[i]) == char.ToLower(polymer[i + 1])) &&
+                    (polymer[i] != polymer[i + 1])) 
+                {
+                    triggered = true;
+                    polymer[i] = Destroyed;
+                    polymer[i + 1] = Destroyed;
+                    i++;
+                }
+            }
+
+            return polymer.Where(unit => unit != Destroyed)
+                .ToArray();
+        }
+
+        static char[] FullyReact(char[] polymer)
+        {
+            bool triggered = false;
+            do
+            {
+                polymer = Trigger(polymer, out triggered);
+            } while (triggered);
+
+            return polymer;
+        }
+
+        static int FindShortestPolymer(char[] polymer)
+        {
+            polymer = FullyReact(polymer);
+
+            char[] alphabet = new char[26];
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                alphabet[i] = (char)(i + 65);
+            }
+
+            return alphabet.Select(letter =>
+            {
+                var unblocked = polymer.Where(unit => char.ToUpper(unit) != letter)
+                    .ToArray();
+
+                return FullyReact(unblocked).Length;
+            }).Min();
+        }
+    }
+}
